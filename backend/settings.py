9a62bef9c6c3eca7b22fce8b0045604f3dbca8ff
Roger McCann <rmccann@ringrock.net>
2024-12-17 20:16:28 -0500

    Update settings.py
    updated static vars

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Audrey"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Hi!"
+    chat_description: str = "I'm Audrey and I'm ready to chat!"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
